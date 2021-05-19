941d6d31c0fc4a1037c87337b2abee71865bc024
Franz Holzinger <holzinger@ttproducts.de>
2021-05-19 11:14:40 +0200

    composer

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = array(
     'title' => 'SVG Library',
     'description' => 'A library to read, parse and export to PDF SVG files.',
     'category' => 'misc',
-    'version' => '0.0.5',
+    'version' => '0.0.6',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'php' => '5.4.0-7.3.99',
-            'typo3' => '6.2.0-9.5.99'
+            'typo3' => '6.2.0-10.4.99'
         ),
         'conflicts' => array(
         ),
