527c59fdce3068745096004116f998750a89bea5
Franz Holzinger <franz.holzinger@festwein.de>
2022-01-22 18:15:07 +0100

    * fix composer.json * support for PHP 8 and TYPO3 11.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'SVG Library',
     'description' => 'A library to read, parse and export to PDF SVG files.',
     'category' => 'misc',
-    'version' => '0.0.6',
+    'version' => '0.1.0',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
@@ -18,8 +18,8 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'jambage.com',
     'constraints' => [
         'depends' => [
-            'php' => '5.4.0-7.3.99',
-            'typo3' => '6.2.0-10.4.99'
+            'php' => '7.4.0-8.1.99',
+            'typo3' => '9.5.0-11.5.99'
         ],
         'conflicts' => [
         ],
