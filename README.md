1d2e52bf1f3a4bd8120ecbe918c99d236625e157
Franz Holzinger <franz.holzinger@festwein.de>
2023-05-06 23:41:37 +0200

    Support for TYPO3 12

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-base_svg
-========
-This provides the SVG file parsing / rendering library.
-It is required as option for dompdf which is called from phpOffice.
+# TYPO3 extension base_svg
+
+## What is does
+
+This extension brings the "SVG file parsing / rendering library" PHP classes "phenx/php-svg-lib" to TYPO3.
+
+## What is the origin
+
+See https://github.com/dompdf/php-svg-lib/tree/0.5.0
+
+## Documentation
+
+https://github.com/dompdf/php-svg-lib/tree/0.5.0
 
 
--- ext_emconf.php
+++ ext_emconf.php
@@ -8,18 +8,15 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'SVG Library',
     'description' => 'A library to read, parse and export to PDF SVG files.',
     'category' => 'misc',
-    'version' => '0.1.0',
+    'version' => '0.5.0',
     'state' => 'stable',
-    'uploadfolder' => 0,
-    'createDirs' => '',
     'clearcacheonload' => 1,
     'author' => 'Fabien Ménager, Franz Holzinger',
     'author_email' => 'franz@ttproducts.de',
     'author_company' => 'jambage.com',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-8.1.99',
-            'typo3' => '9.5.0-11.5.99'
+            'typo3' => '10.4.0-12.4.99'
         ],
         'conflicts' => [
         ],
